6e0e8e4057874ac6c4f09a1afee7c99b7fb1b4ce
JunJun <hello@jaysan.io>
2021-06-27 21:45:07 +0900

    hahaha

--- helloworldapp/views.py
+++ helloworldapp/views.py
@@ -1,15 +1,16 @@
 # from .models import Post
-from django.views import generic
-from django.views.generic import View
-from django.contrib.auth.mixins import LoginRequiredMixin
 import logging
 import requests
 import sys
 import io
+from django.views import generic
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from braces.views import JSONResponseMixin
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
 # 日本語を受信時にエラーにならないようにする為に必要。
@@ -26,12 +27,12 @@ def get(request):
 
         # key = 'AIzaSyCAXFvYgTVg6ivXm6xrT2It3VjzgOFF6no' # TEXTEMOTION_KEY
         # url = f'https://language.googleapis.com/v1/documents:analyzeSentiment?key={key}' # TEXTEMOTION_URL
-        
+
         # key = settings.TEXTEMOTION_KEY
         # url = settings.TEXTEMOTION_URL
-        
+
         url = settings.TEXTEMOTION_URL
-        
+
         header = {'Content-Type': 'application/json'}
 
         body = {
