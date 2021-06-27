20529dc2edb282a85dcc7f2c58d352a47917cbe1
JunJun <hello@jaysan.io>
2021-06-27 20:42:56 +0900

    changed_file_only_2021-06-27-20

--- helloworldapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from . import views
-
-app_name = 'text'
-urlpatterns = [
-    path('', views.helloworldfunction, name="text"),
-    path('get/', views.get, name="get"),
-]
--- static/js/auth.js
+++
@@ -1,4 +0,0 @@
-let issuerURLHide = "https://acp-api.amivoice.com/issue_service_authorization";
-let authorizationHide = 'FFFFE8D6D3E73ACF35928DD1A29A24083F7D2CB08E4B2799BCDF00987990D80975';
-let sidHide = 'JunJun';
-let spwHide = '8qYaMwXTjZ3dNszg';
